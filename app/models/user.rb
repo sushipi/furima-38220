5096ceeb208769e984288ba26a377240709aab0b
sushipi <watachans1996@gmail.com>
2022-10-15 15:55:16 +0900

    ユーザー管理機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+        
+  validates :nickname, presence: true
+  validates :family_name, presence: true, format: { with: /\A[一-龥ぁ-ん]/ }
+  validates :given_name, presence: true, format: { with: /\A[一-龥ぁ-ん]/ }
+  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :given_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :birth_date, presence: true
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+
+  has_many :items
+  has_many :purchases
+  has_many :comments       
 end
