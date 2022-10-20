534c3b259d398709e83f32e1af1691cee706f3a9
sushipi <watachans1996@gmail.com>
2022-10-20 19:58:21 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'items/index'
+  resources :items, only: [:index, :new, :create]
   root to: "items#index"
+  post "/items/new" => "items#create"
 end
