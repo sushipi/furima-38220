a1328146eb1cb41fa7eda1789bc11bc07f7fa675
sushipi <watachans1996@gmail.com>
2022-10-20 19:57:47 +0900

    バリデーション設定

---
+++ app/models/item.rb
@@ -0,0 +1,24 @@
+class Item < ApplicationRecord
+  with_options numericality: { other_than: 1, message: "can't be blank" } do 
+    validates :category_id
+    validates :condition_id
+    validates :delivery_fee_id
+    validates :prefecture_id
+    validates :day_id 
+  end
+
+  validates :name, presence: true
+  validates :explanation, presence: true
+  validates :image, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, format: { with: /\A[0-9]+\z/ }
+
+  has_one_attached :image
+  belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_fee
+  belongs_to :prefecture
+  belongs_to :day  
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX }
 
-  #has_many :items
+  has_many :items
   #has_many :purchases
   #has_many :comments       
 end
