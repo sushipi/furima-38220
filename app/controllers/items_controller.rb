542b3e0f3f7aa814dea95c4abaf35e493da130f0
sushipi <watachans1996@gmail.com>
2022-10-24 19:12:18 +0900

    コントローラー

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -22,8 +23,29 @@ class ItemsController < ApplicationController
       @item = Item.find(params[:id])
   end 
 
+  def edit 
+    @item = Item.find(params[:id])
+  end
+
+  def update 
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end    
+  
+  end
+
   private
   def item_params
     params.require(:item).permit(:name, :image, :explanation, :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to action: :index
+    end
   end
 end
