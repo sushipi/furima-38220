3febc431989ab39739e048402c40a0eae6246c50
sushipi <watachans1996@gmail.com>
2022-10-24 19:12:34 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :items, only: [:index, :new, :create, :show]
+  resources :items, only: [:index, :new, :create, :show, :edit, :update]
   root to: "items#index"
+  #post "items/:id" => "items#update"
 end
